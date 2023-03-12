98b798ef4a28c6b1f0822e9b8df28ee56a4b8822
Mashotolucky <mashotolucky99@gmail.com>
2023-03-12 23:57:30 +0200

    backende is live

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -5,8 +5,8 @@ const { Pool } = require("pg");
 const isProduction = process.env.NODE_ENV === "production";
 const database = process.env.NODE_ENV === "dev" ? process.env.PGDATABASE_TEST : process.env.PGDATABASE;
 
-const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${database}`;
-//const connectionString = `postgresql://root:12345@localhost:5432/esign`;
+// const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${database}`;
+const connectionString = `postgres://server_tut:Gd2HkVpDqaxBCcR06NmHteSQ6dQMk6E0@dpg-cg6tv0d269v5l67fp5bg-a.oregon-postgres.render.com/campusdb`;
 
 const pool = new Pool({
   connectionString: isProduction? process.env.DATABASE_URL : connectionString,
